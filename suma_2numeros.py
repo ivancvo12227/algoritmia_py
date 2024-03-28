eeab4bb49afdc77031a396777c4eb6801db25e59
ivancvo12227 <150964574+ivancvo12227@users.noreply.github.com>
2024-03-28 16:36:13 -0500

    Create suma_2numeros.py

---
+++ suma_2numeros.py
@@ -0,0 +1,5 @@
+inicio = int(input("este es un algoritmo que hace la suma de 2 numeros para continuar digite un numero:  "))
+suma = int(input("digite su primer numero: "))
+suma2 = int(input("digite el segundo numero: "))
+resultado = suma + suma2
+print ("su resultado es: ", resultado)
